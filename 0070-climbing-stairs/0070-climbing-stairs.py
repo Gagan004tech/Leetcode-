8d129693a4c6482e813705f9bc2468e504aec2de
Gagandeep M <gagandeepgcsj2023@gmail.com>
2026-07-24 19:23:25 +0530

    Time: 0 ms (100%), Space: 19.3 MB (51.83%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.py
+++ 0070-climbing-stairs/0070-climbing-stairs.py
@@ -1,13 +1,19 @@
 class Solution:
     def climbStairs(self, n: int) -> int:
-        if n <=2 :
-            return n
-        prev1 =1
-        prev2 =2
-        for i in range(3,n+1):
-            temp =prev1+prev2
-            prev1,prev2 = prev2,temp
-        return prev2
+        # if n <=2 :
+        #     return n
+        # prev1 =1
+        # prev2 =2
+        # for i in range(3,n+1):
+        #     temp =prev1+prev2
+        #     prev1,prev2 = prev2,temp
+        # return prev2
+        one,two = 1,1
+        for i in range(n-1):
+            temp=one
+            one=one+two
+            two=temp
+        return one
 '''
 Here Time Complexity:= O(n)
 Here Space Complexity:= O(1)
